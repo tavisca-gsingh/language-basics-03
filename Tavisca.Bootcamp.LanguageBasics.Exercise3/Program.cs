a75839e013fc0cb06c9b2bcceed358eb847c988d
Gursimran Singh <gsingh@tavisca.in>
2019-06-28 18:39:25 +0530

    Completed SelectMeals Function

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -29,7 +31,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         }
 
         private static void Test(int[] protein, int[] carbs, int[] fat, string[] dietPlans, int[] expected)
-        {
+        {   
             var result = SelectMeals(protein, carbs, fat, dietPlans).SequenceEqual(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"Proteins = [{string.Join(", ", protein)}]");
             Console.WriteLine($"Carbs = [{string.Join(", ", carbs)}]");
@@ -38,9 +40,75 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
+
+        public static List<int> FindMinIndex(List<int> mealList,int[] category){
+            var elements=new List<int>();
+            foreach(int index in mealList){
+                elements.Add(category[index]);
+            }
+            int min=elements.Min();
+            elements.Clear();
+            foreach(int index in mealList){
+                if (category[index]==min){
+                    elements.Add(index);
+                }
+            }
+            return elements;
+        }
+
+        public static List<int> FindMaxIndex(List<int> mealList,int[] category){
+            var elements=new List<int>();
+            foreach(int index in mealList){
+                elements.Add(category[index]);
+            }
+            int min=elements.Max();
+            elements.Clear();
+            foreach(int index in mealList){
+                if (category[index]==min){
+                    elements.Add(index);
+                }
+            }
+            return elements;
+        }
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
-        {
-            // Add your code here.
+        {   int length=protein.Length;
+            int[] calories=new int[length];
+            int i;
+            for(i=0;i<length;i++){
+                calories[i]=protein[i]*5+carbs[i]*5+fat[i]*9;
+            }
+            int[] selectedDietPlans=new int[dietPlans.Length];
+            List<int> selectedDietPlan=new List<int>();
+            i=0;
+            foreach(var dietplan in dietPlans){
+                selectedDietPlan=Enumerable.Range(0,length).ToList();
+                foreach(var element in dietplan){
+                    if(element=='C'){
+                        selectedDietPlan=FindMaxIndex(selectedDietPlan,carbs);
+                    }
+                    else if(element=='c'){
+                        selectedDietPlan=FindMinIndex(selectedDietPlan,carbs);
+                    }else if(element=='P'){
+                        selectedDietPlan=FindMaxIndex(selectedDietPlan,protein);
+                    }else if(element=='p'){
+                        selectedDietPlan=FindMinIndex(selectedDietPlan,protein);
+                    }else if(element=='F'){
+                        selectedDietPlan=FindMaxIndex(selectedDietPlan,fat);
+                    }else if(element=='f'){
+                        selectedDietPlan=FindMinIndex(selectedDietPlan,fat);
+                    }else if(element=='T'){
+                        selectedDietPlan=FindMaxIndex(selectedDietPlan,calories);
+                    }else if(element=='t'){
+                        selectedDietPlan=FindMinIndex(selectedDietPlan,calories);
+                    }
+                    if(selectedDietPlan.Count==1){
+                        break;
+                    }
+                }
+                selectedDietPlans[i]=selectedDietPlan[0];
+                i+=1;
+            }
+            return selectedDietPlans;
             throw new NotImplementedException();
         }
     }
